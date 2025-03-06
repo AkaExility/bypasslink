4e2ce68052eef293bfe4e121c031094c4f774b11
AkaExility <129511483+AkaExility@users.noreply.github.com>
2025-03-06 16:56:07 +0700

    tampertext.js

--- tampertext.js
+++ tampertext.js
@@ -2,7 +2,7 @@
 // @name         Bypass Private
 // @namespace    http://tampermonkey.net/
 // @version      3.3.1
-// @description  Bypass Yeumoney premium
+// @description  Bypass Yeumoney For Aka Only
 // @require      https://github.com/AkaExility/bypasslink/blob/main/maincharacter.js
 // @updateURL    https://github.com/AkaExility/bypasslink/blob/main/tampertext.js
 // @downloadURL  https://github.com/AkaExility/bypasslink/blob/main/tampertext.js
